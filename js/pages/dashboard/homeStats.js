1f5571d35558948d7d470a30dc71c454cd4cfe54
Jason Cleeland <jason@cleeland.org>
2021-06-10 16:36:11 +1000

    Fixed & enhanced home statistics page
    Fixed bugs, added site statistics

--- js/pages/dashboard/homeStats.js
+++ js/pages/dashboard/homeStats.js
@@ -97,6 +97,9 @@ function loadCaseDepartments() {
     
     $.when(statsCases(parameters, conditions, order, first, last, select)).done(function(stats) {
         //console.log(graph);
+        if(stats.count == 0) {
+            graph.push(['', 0, colors[colorCount]]);
+        }
         $.each(stats.results, function(i, result) {
             graph.push([result.category_name, parseInt(result.qty), colors[colorCount]]);
             colorCount++;
@@ -133,13 +136,16 @@ function loadCaseTypes() {
     
     $.when(statsCases(parameters, conditions, order, first, last, select)).done(function(stats) {
         //console.log(graph);
+        if(stats.count==0) {
+            graph.push(['', 0, colors[colorCount]]);
+        }
         $.each(stats.results, function(i, result) {
             graph.push([result.tasktype_name, parseInt(result.qty), colors[colorCount]]);
             colorCount++;
             if(colorCount > 3) colorCount=0;    
         })
         google.charts.setOnLoadCallback(function () {
-            //console.log(graph);
+            console.log(graph);
             googleMultiBarChart('dashboardStatistics', 'My Case Types', graph);
                             
         });
@@ -204,16 +210,16 @@ function loadCaseStats() {
         
 
         if(item=='Open Cases') {
-            conditions='t.assigned_to = :assigned_to AND date_opened <= :start_time AND (date_closed = "" OR date_closed IS NULL OR date_closed > :end_time)';
+            conditions='t.assigned_to = :assigned_to AND date_opened <= :start_time AND (date_closed = "" OR date_closed IS NULL OR date_closed > :end_time) AND :title=:title';
         }
         if(item=='New Cases') {
-            conditions='t.assigned_to = :assigned_to AND date_opened >= :start_time AND date_opened <=:end_time';
+            conditions='t.assigned_to = :assigned_to AND date_opened >= :start_time AND date_opened <=:end_time AND :title=:title';
         }
         if(item=='Closed Cases') {
-            conditions='t.assigned_to = :assigned_to AND date_closed >= :start_time AND date_closed <= :end_time';
+            conditions='t.assigned_to = :assigned_to AND date_closed >= :start_time AND date_closed <= :end_time AND :title=:title';
         }
 
-        var iterations=parseInt(((startTimeStamp-earlydate)/(86400*7))+2);
+        var iterations=parseInt(((startTimeStamp-earlydate)/(86400*7))+1);
         
         for(t=earlydate; t<=startTimeStamp; t+=(86400*7)) {
             //check for daylight savings issues
@@ -227,6 +233,7 @@ function loadCaseStats() {
             (function(tindex) {
                 parameters[':start_time']=tindex;
                 parameters[':end_time']=(tindex+(86400*14)-1);
+                parameters[':title']=item;
                 //console.log(parameters);
                 $.when(statsCases(parameters, conditions, order, first, last, null)).done(function(stats) {
                     var stat=stats.results[0]['total'];
@@ -238,8 +245,19 @@ function loadCaseStats() {
                     if(typeof graph[tindex]=='undefined') {
                         graph[tindex]=[];
                         graph[tindex].push(timestamp2date(tindex, "dd MM"));
+                        graph[tindex].push(null);
+                        graph[tindex].push(null);
+                        graph[tindex].push(null);
                     }
-                    graph[tindex].push(parseInt(stats.results[0]['total']));
+                    //console.log(stats.parameters[":title"]);
+                    if(stats.parameters[":title"]=="Open Cases") {
+                        graph[tindex][1]=parseInt(stats.results[0]['total']);
+                    } else if(stats.parameters[":title"]=="Closed Cases") {
+                        graph[tindex][2]=parseInt(stats.results[0]['total']);
+                    } else if(stats.parameters[":title"]=="New Cases") {
+                        graph[tindex][3]=parseInt(stats.results[0]['total']);
+                    }
+                    //graph[tindex].push(parseInt(stats.results[0]['total']));
                     //console.log(graph);
                     iterationcount++;
                     //console.log('Iterations: '+iterationcount+' / '+iterations);
@@ -264,6 +282,284 @@ function loadCaseStats() {
                                     });
                                 //console.log(results);
                                 //console.log(graph);                    
+                            }
+                            
+                        }                      
+                })
+            })(t);
+
+
+           
+        }        
+        
+        
+    })
+    
+    
+    
+    
+    
+}
+
+
+function loadAllCaseDates() {
+    $('#dashboardStatistics').html('<img src="images/logo_flip.gif" />');
+    
+    var parameters={};
+    //parameters[':assigned_to']=globals.user_id;
+    var conditions='is_closed != 1';
+
+    var now = new Date; // now
+    now.setHours(0);   // set hours to 0
+    now.setMinutes(0); // set minutes to 0
+    now.setSeconds(0); // set seconds to 0
+
+    var startTimeStamp = Math.floor(now / 1000);
+    var endTimeStamp=startTimeStamp+86399;
+    
+    parameters[':startTimeStamp1']=startTimeStamp;
+    parameters[':startTimeStamp2']=startTimeStamp;
+    parameters[':startTimeStamp3']=startTimeStamp;
+    parameters[':startTimeStamp4']=startTimeStamp;
+    parameters[':endTimeStamp1']=endTimeStamp;
+    parameters[':endTimeStamp2']=endTimeStamp;
+    
+    var select='CASE WHEN t.date_due < :startTimeStamp1 THEN "overdue" WHEN t.date_due >= :startTimeStamp2 AND t.date_due <= :endTimeStamp1 THEN "current" ELSE "future" END as status, count(*) as qty';
+    
+    var order = 'GROUP BY CASE WHEN t.date_due < :startTimeStamp3 THEN "overdue" WHEN t.date_due >= :startTimeStamp4 AND t.date_due <= :endTimeStamp2 THEN "current" ELSE "future" END';
+    
+    var first=0;
+    var last=1;
+    
+    var graph=[];
+    graph[0]=['Time status', 'Cases'];
+    
+    
+    $.when(statsCases(parameters, conditions, order, first, last, select)).done(function(stats) {
+        //console.log(graph);
+        $.each(stats.results, function(i, result) {
+            graph.push([result.status, parseInt(result.qty)]);
+        })
+        google.charts.setOnLoadCallback(function () {
+            console.log(graph);
+            googlePieChart('dashboardStatistics', 'Case Status Overview', graph);
+                            
+        });
+    })    
+    
+}
+
+function loadAllCaseDepartments() {
+    $('#dashboardStatistics').html('<img src="images/logo_flip.gif" />');
+    
+    var parameters={};
+    //parameters[':assigned_to']=globals.user_id;
+    var conditions='is_closed != 1';
+    
+    var select='lc.category_name, count(*) as qty';
+    
+    var order = 'group by lc.category_name'; //put in a group by calc to show the case types.
+    var first=0;
+    var last=1;
+    
+    var results={};
+    results['Case Types']={};
+    var graph=[];
+    graph[0]=['Type', 'Cases', {role: 'style'}];
+
+    colorCount=0;
+
+    
+    $.when(statsCases(parameters, conditions, order, first, last, select)).done(function(stats) {
+        //console.log(graph);
+        if(stats.count == 0) {
+            graph.push(['', 0, colors[colorCount]]);
+        }
+        $.each(stats.results, function(i, result) {
+            graph.push([result.category_name, parseInt(result.qty), colors[colorCount]]);
+            colorCount++;
+            if(colorCount > 3) colorCount=0;    
+        })
+        google.charts.setOnLoadCallback(function () {
+            console.log(graph);
+            googleMultiBarChart('dashboardStatistics', 'Case Departments', graph);
+                            
+        });
+    })
+}
+
+function loadAllCaseTypes() {
+    $('#dashboardStatistics').html('<img src="images/logo_flip.gif" />');
+    
+    var parameters={};
+    //parameters[':assigned_to']=globals.user_id;
+    var conditions='is_closed != 1';
+    
+    var select='lt.tasktype_name, count(*) as qty';
+    
+    var order = 'group by lt.tasktype_name'; //put in a group by calc to show the case types.
+    var first=0;
+    var last=1;
+    
+    var results={};
+    results['Case Types']={};
+    var graph=[];
+    graph[0]=['Type', 'Cases', {role: 'style'}];
+
+    colorCount=0;
+
+    
+    $.when(statsCases(parameters, conditions, order, first, last, select)).done(function(stats) {
+        //console.log(graph);
+        if(stats.count==0) {
+            graph.push(['', 0, colors[colorCount]]);
+        }
+        $.each(stats.results, function(i, result) {
+            graph.push([result.tasktype_name, parseInt(result.qty), colors[colorCount]]);
+            colorCount++;
+            if(colorCount > 3) colorCount=0;    
+        })
+        google.charts.setOnLoadCallback(function () {
+            console.log(graph);
+            googleMultiBarChart('dashboardStatistics', 'Case Types', graph);
+                            
+        });
+    })
+}
+
+function loadAllCaseStats() {
+    $('#dashboardStatistics').html('<img src="images/logo_flip.gif" />');
+    //Currently Open Cases for this User
+    var parameters={};
+    //parameters[':assigned_to']=globals.user_id;
+    var conditions='is_closed != 1';
+    var order=null;
+    var first=0;
+    var last=1;
+    
+    /* $.when(statsCases(parameters, conditions, order, first, last)).done(function(cases) {
+        //console.log(cases);
+    }) */
+    
+    
+    //Generate 3 months of open case info by fortnight
+    var now = new Date; // now
+        now.setHours(0);   // set hours to 0
+        now.setMinutes(0); // set minutes to 0
+        now.setSeconds(0); // set seconds to 0
+
+    var startTimeStamp = Math.floor(now / 1000)
+    //console.log('Starttime: '+timestamp2date(startTimeStamp, "yy/mm/dd g:i a"));
+    
+    var endTimeStamp=startTimeStamp+86399;
+    var fortnight=86400*14;
+    var earlydate=startTimeStamp-(fortnight*6);
+    //check for daylight savings issues
+    var ds1check=new Date(earlydate*1000);
+    if(ds1check.getHours()==23) {
+        earlydate+=3600;
+    }
+    if(ds1check.getHours()==1) {
+        earlydate-=3600;
+    }
+    //console.log('Earlydate: ['+earlydate+'] '+timestamp2date(earlydate, "yy/mm/dd g:i a"));
+    //console.log(startTimeStamp);
+    //conditions='t.assigned_to = :assigned_to AND date_opened <= :start_time AND (date_closed = "" OR date_closed IS NULL OR date_closed > :end_time)';
+    
+    var results={};
+    results['Open Cases']={};
+    results['New Cases']={};
+    results['Closed Cases']={};
+    graph={};
+    graph[0]=['Date', 'Open Cases', 'Closed Cases', 'New Cases'];
+    var resultstotal=0;
+    var iterationcount=0;
+    var resultscount=0;
+    $.each(results, function(i,b) {
+        resultstotal++;
+    })
+    
+    
+    
+    $.each(results, function(item, contents) {
+        
+        //console.log('Doing '+item);
+        //Note the ":title = :title" is a hack/trick designed to return the item being run in the parameter section, to avoid confusion in compilation of results
+        if(item=='Open Cases') {
+            conditions='date_opened <= :start_time AND (date_closed = "" OR date_closed IS NULL OR date_closed > :end_time) AND :title=:title';
+        }
+        if(item=='New Cases') {
+            conditions='date_opened >= :start_time AND date_opened <=:end_time AND :title=:title';
+        }
+        if(item=='Closed Cases') {
+            conditions='date_closed >= :start_time AND date_closed <= :end_time AND :title=:title';
+        }
+
+        var iterations=parseInt(((startTimeStamp-earlydate)/(86400*7))+1);
+        for(t=earlydate; t<=startTimeStamp; t+=(86400*7)) {
+            //check for daylight savings issues
+            var ds2check=new Date(t*1000);
+            if(ds2check.getHours()==23) {
+                t+=3600;
+            }
+            if(ds2check.getHours()==1) {
+                t-=3600;
+            }
+            (function(tindex) {
+                parameters[':start_time']=tindex;
+                parameters[':end_time']=(tindex+(86400*14)-1);
+                parameters[':title']=item;
+                
+                //console.log(parameters);
+                                    
+                $.when(statsCases(parameters, conditions, order, first, last, null)).done(function(stats) {
+                    var stat=stats.results[0]['total'];
+                    var n=new Date(tindex*1000);
+                    var label=n.getFullYear()+'/'+('0'+n.getMonth()).slice(-2)+'/'+('0'+n.getDate()).slice(-2); 
+                    var label=timestamp2date(tindex, "yy-mm-dd");
+                    results[item][label]=[];
+                    results[item][label]=parseInt(stats.results[0]['total']);
+                    if(typeof graph[tindex]=='undefined') {
+                        graph[tindex]=[];
+                        graph[tindex].push(timestamp2date(tindex, "dd MM"));
+                        graph[tindex].push(null);
+                        graph[tindex].push(null);
+                        graph[tindex].push(null);
+                    }
+                    console.log(stats.parameters[":title"]);
+                    if(stats.parameters[":title"]=="Open Cases") {
+                        graph[tindex][1]=parseInt(stats.results[0]['total']);
+                    } else if(stats.parameters[":title"]=="Closed Cases") {
+                        graph[tindex][2]=parseInt(stats.results[0]['total']);
+                    } else if(stats.parameters[":title"]=="New Cases") {
+                        graph[tindex][3]=parseInt(stats.results[0]['total']);
+                    }
+                    //graph[tindex].push(parseInt(stats.results[0]['total']));
+                    //console.log(graph);
+                    iterationcount++;
+                    //console.log('Iterations: '+iterationcount+' / '+iterations);
+                    //console.log(parseInt(stats.results[0]['total']));
+                }).then(function() {
+                        if(iterationcount==iterations) {
+                            //We've finished this subset
+                            resultscount++;
+                            iterationcount=0;
+                            //console.log('Results Count: '+resultscount);
+                            if(resultscount==resultstotal) {
+                                    var grapharray=[];
+                                    //console.log(results);
+                                    //console.log(graph);                    
+
+                                    $.each(graph, function(a,b) {
+                                        console.log(a+'-'+b);
+                                        grapharray.push(b);
+                                    })
+                                    //var grapharray=Object.entries(graph);
+                                    google.charts.setOnLoadCallback(function () {
+                                        //drawLineChart(z, y, 'dashboardStatistics', 'Date', 'Cases', null, null, 'none');
+                                        googleMultiLineChart('dashboardStatistics', 'My Cases', grapharray);
+                                                        
+                                    });
                             }
                             
                         }                      
--- pages/dashboard/statistics.php
+++ pages/dashboard/statistics.php
@@ -2,10 +2,18 @@
 <h4 class="header">Statistics</h4>
 <div class="pager rounded-bottom">&nbsp;
     <select id='statsChooser' name='statsChooser' class='form-control-xs float-right p0' style='font-size: 8pt !important;'>
-        <option value='CaseStats'>My cases</option>
-        <option value='CaseDepartments'>My case departments</option>
-        <option value='CaseTypes'>My case types</option>
-        <option value='CaseDates'>My case statuses</option>
+        <optgroup label='My stats'>
+            <option value='CaseStats'>Case Load</option>
+            <option value='CaseDepartments'>Case departments</option>
+            <option value='CaseTypes'>Case types</option>
+            <option value='CaseDates'>Case statuses</option>
+        </optgroup>
+        <optgroup label='Site stats'>
+            <option value='AllCaseStats'>Case Load</option>
+            <option value='AllCaseDepartments'>Case departments</option>
+            <option value='AllCaseTypes'>Case types</option>
+            <option value='AllCaseDates'>Case statuses</option>
+        </optgroup>
     </select>
 </div>
 <div class="row border rounded bg-light justify-content-sm-center m-1 w-100">
