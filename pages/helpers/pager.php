b327ce0cd45d1b530876fd6dfdf39acdda5cee42
Jason Cleeland <jason@cleeland.org>
2021-11-04 12:40:52 +1100

    Tidying of css around pager

--- pages/helpers/pager.php
+++ pages/helpers/pager.php
@@ -24,11 +24,11 @@ $(document).ready(function(){
 
 </script>
 <div class='pager rounded-bottom'>&nbsp;
-    <div class="float-right mr-1 ml-0 small pointer pagerbutton" id="<?php echo $pagername ?>last" title='last page' value=''><img src='images/end.svg' /></div>
-    <div class="float-right mr-0 ml-0 small pointer pagerbutton" id="<?php echo $pagername ?>end" title='next page' value='9'><img src='images/chevron-right.svg' /></div>
-    <div class="float-right m-0 p-0 small pagerposition" id="<?php echo $pagername ?>position"></div>
-    <div class="float-right ml-0 mr-0 small pointer pagerbutton" id="<?php echo $pagername ?>start" title='last page' value='0'><img src='images/chevron-left.svg' /></div>
-    <div class="float-right ml-1 mr-0 small pointer pagerbutton" id="<?php echo $pagername ?>first" title='first page' value=''><img src='images/start.svg' /></div>
+    <div class="float-right mr-1 ml-0 pl-1 pr-1 small pointer pagerbutton" id="<?php echo $pagername ?>last" title='last page' value=''><img src='images/end.svg' /></div>
+    <div class="float-right mr-0 ml-0 pl-1 pr-1 small pointer pagerbutton" id="<?php echo $pagername ?>end" title='next page' value='9'><img src='images/chevron-right.svg' /></div>
+    <div class="float-right m-0 p-0 pl-1 pr-1 small pagerposition" id="<?php echo $pagername ?>position"></div>
+    <div class="float-right ml-0 mr-0 pl-1 pr-1 small pointer pagerbutton" id="<?php echo $pagername ?>start" title='previous page' value='0'><img src='images/chevron-left.svg' /></div>
+    <div class="float-right ml-1 mr-0 pl-1 pr-1 small pointer pagerbutton" id="<?php echo $pagername ?>first" title='first page' value=''><img src='images/start.svg' /></div>
     <div class="float-right mr-1 pl-1 pr-1 text-muted small rounded pagerlight" id="<?php echo $pagername ?>total"></div>
     <div class="float-right m1-1 pl-1 pr-0 text-muted small rounded pagerlight"><input class="text-muted small pagerlight ml-1 pl-1 pr-0 pt-1" style="width: 18px; border: 0" id="<?php echo $pagername ?>qty" title="Quantity shown" value='10' /></div>
     
